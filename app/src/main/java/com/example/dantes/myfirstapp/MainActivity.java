73e8aec72b1edaf3f13baa50ebf3c6b26aad991e
pmdantes <pmdantes@gmail.com>
2016-05-18 00:12:54 -0700

    Initial commit
    Finished building my first app using Android development

---
+++ app/src/main/java/com/example/dantes/myfirstapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.dantes.myfirstapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity
+{
+    // Public constant
+    public final static String EXTRA_MESSAGE = "com.example.dantes.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /* Called when the user clicks the Send button */
+    public void sendMessage(View view)
+    {
+        // Creates new Intent object used to send action
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+
+        // Creates new EditText object by getting the EditText element
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        // Get text and store into String object
+        String message = editText.getText().toString();
+
+        // Intent object carry data types as key-value pairs called extras
+        // putExtra() takes key as first parameter and value in the second parameter
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+    }
+}
